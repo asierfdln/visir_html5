60a48ec5aea99c6e21cf642b974af60bc094b457
unknown <asierfdln@gmail.com>
2019-04-24 12:19:10 +0200

    Handlers added

--- instruments/tripledc/tripledc.js
+++ instruments/tripledc/tripledc.js
@@ -11,6 +11,7 @@ visir.TripleDC = function(id, elem)
 	this._elem = elem;
 
 	this._config = "nothing";
+	this.channels = ["6V+", "25V+", "25V-"];
 	this.boundaries = ["noboundary", "noboundary", "noboundary"]; // 6V+, 25V+, 25V-
 
 	// all the values are represented times 1000 to avoid floating point trouble
@@ -202,47 +203,46 @@ visir.TripleDC.prototype.ReadResponse = function(response) {
 	this._UpdateDisplay(true);
 }
 
-var channels = ["6V+", "25V+", "25V-"];
 var errors = ["", "", ""];
 visir.TripleDC.prototype.LoadHandler = function (config) {
 	if (config !== "nothing") {
 		this._config = config;
 		errors = ["", "", ""];
-		for (var i = 0; i < channels.length; i++) {
-			if (config.tripledc_prohbChannels.includes(this._activeChannel) && this._activeChannel == channels[i]) {
+		for (var i = 0; i < this.channels.length; i++) {
+			if (config.tripledc_prohbChannels.includes(this._activeChannel) && this._activeChannel == this.channels[i]) {
 				errors[i] += "This experiment does not support this channel (" + this._activeChannel + ").";
 			} else {
-				if (this._values[channels[i]].voltage / 1000 > config.tripledc_max[channels[i]]) {
+				if (this._values[this.channels[i]].voltage / 1000 > config.tripledc_max[this.channels[i]]) {
 					if (this.boundaries[i] == "noboundary") {
-						errors[i] += "This experiment only admits values between " + config.tripledc_min[channels[i]] + " and "
-							+ config.tripledc_max[channels[i]] + " for the channel " + channels[i] + ".";
-						this.boundaries[i] = this._values[channels[i]].voltage / 1000;
+						errors[i] += "This experiment only admits values between " + config.tripledc_min[this.channels[i]] + " and "
+							+ config.tripledc_max[this.channels[i]] + " for the channel " + this.channels[i] + ".";
+						this.boundaries[i] = this._values[this.channels[i]].voltage / 1000;
 					} else {
-						if (!(this._values[channels[i]].voltage / 1000 < this.boundaries[i])) {
-							errors[i] += "This experiment only admits values between " + config.tripledc_min[channels[i]] + " and "
-								+ config.tripledc_max[channels[i]] + " for the channel " + channels[i] + ".";
+						if (!(this._values[this.channels[i]].voltage / 1000 < this.boundaries[i])) {
+							errors[i] += "This experiment only admits values between " + config.tripledc_min[this.channels[i]] + " and "
+								+ config.tripledc_max[this.channels[i]] + " for the channel " + this.channels[i] + ".";
 						}
-						this.boundaries[i] = this._values[channels[i]].voltage / 1000;
+						this.boundaries[i] = this._values[this.channels[i]].voltage / 1000;
 					}
-				} else if (this._values[channels[i]].voltage / 1000 < config.tripledc_min[channels[i]]) {
+				} else if (this._values[this.channels[i]].voltage / 1000 < config.tripledc_min[this.channels[i]]) {
 					if (this.boundaries[i] == "noboundary") {
-						errors[i] += "This experiment only admits values between " + config.tripledc_min[channels[i]] + " and "
-							+ config.tripledc_max[channels[i]] + " for the channel " + channels[i] + ".";
-						this.boundaries[i] = this._values[channels[i]].voltage / 1000;
+						errors[i] += "This experiment only admits values between " + config.tripledc_min[this.channels[i]] + " and "
+							+ config.tripledc_max[this.channels[i]] + " for the channel " + this.channels[i] + ".";
+						this.boundaries[i] = this._values[this.channels[i]].voltage / 1000;
 					} else {
-						if (!(this._values[channels[i]].voltage / 1000 > this.boundaries[i])) {
-							errors[i] += "This experiment only admits values between " + config.tripledc_min[channels[i]] + " and "
-								+ config.tripledc_max[channels[i]] + " for the channel " + channels[i] + ".";
+						if (!(this._values[this.channels[i]].voltage / 1000 > this.boundaries[i])) {
+							errors[i] += "This experiment only admits values between " + config.tripledc_min[this.channels[i]] + " and "
+								+ config.tripledc_max[this.channels[i]] + " for the channel " + this.channels[i] + ".";
 						}
-						this.boundaries[i] = this._values[channels[i]].voltage / 1000;
+						this.boundaries[i] = this._values[this.channels[i]].voltage / 1000;
 					}
 				} else {
 					this.boundaries[i] = "noboundary";
 				}
 			}
 		}
-		if (errors[channels.indexOf(this._activeChannel)] !== "") {
-			alert("[DC Power]\n\n" + errors[channels.indexOf(this._activeChannel)] + "\n\n");
+		if (errors[this.channels.indexOf(this._activeChannel)] !== "") {
+			alert("[DC Power]\n\n" + errors[this.channels.indexOf(this._activeChannel)] + "\n\n");
 		}
 	}
 }
